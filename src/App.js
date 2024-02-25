887f3269a0606a636fd2a59c20aa4ff46e17b62d
lndsilva <lndsilva@hotmail.com>
2024-02-25 19:04:04 -0300

    app react native - trabalhando com projetos iniciais

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+import MinMax from './components/MinMax';
+// import Comp from './components/Mult';
+//import {Comp1, Comp2} from './components/Mult';
+//import Primeiro from './components/Primeiro';
+
+export default () => (
+  <View style={style.App}>
+    <MinMax />
+    {/* <Comp />
+    <Comp1 />
+    <Primeiro />
+  <Comp2 /> */}
+  </View>
+);
+
+const style = StyleSheet.create({
+  App: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+});
---
+++ src/components/MinMax.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Estilo from './estilo';
+
+export default () => (
+  <Text style={Estilo.txtG}>O valor de x é maior que o valor de y</Text>
+);
